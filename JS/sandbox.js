c94620c13beef8a6c012432f31b110699dd21445
FlametheGOD <phuhainguyen7@gmail.com>
2022-06-23 19:40:26 +0700

    `js-update`

---
+++ JS/sandbox.js
@@ -0,0 +1,11 @@
+console.log(`Hellow world! From FTG`)
+let a = 5;
+let b = `5`;
+console.log(`Compare a == b: `, `a= `, a, `b= `, b, `result: `, a == b);
+console.log(`Compare a === b: `, `a= `, a, `b= `, b, `result: `, a === b)
+// "===" type,value covention
+// &&	and	(x < 10 && y > 1) is true
+// ||	or	(x == 5 || y == 5) is false
+// !	not	!(x == y) is true
+let c = null;
+console.log(`check value: `, c);
--- JS/string.js
+++ JS/string.js
@@ -3,7 +3,7 @@ let text = `I hope this is help of Commander,who use sword and bow`
 let newtext = text.replace("sword", "spear"); //replace
 console.log(newtext);
 let a = '1$2$3$4$5$'
-console.log(a.split("$"));//split
+console.log(a.split("$"));//split => 1 2 3 4 5 
 let b = a.slice(0, 4);
 console.log(b);//slice
 console.log(`Hello world! From JS`.toUpperCase())
